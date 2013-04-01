100fe749e9317b07fa1c96e7d71f336b647c9262
JorisSchelfaut <joris.schelfaut@hotmail.com>
2013-04-01 14:41:53 +0200

    removed some comments

--- js/background.js
+++ js/background.js
@@ -14,21 +14,9 @@
  * 
  */
 
-// LISTENERS :
-// -----------
-/* 
- * Listen for any changes to the URL of any tab.
- */
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
-/*
- * The onMessage method is a part of chrome.extension
- * http://developer.chrome.com/extensions/extension.html
- */
 chrome.extension.onMessage.addListener(doAction);
 
-
-// GLOBAL VARIABLES AND CONSTANTS :
-// --------------------------------
 var api_key     = '828c109e6a54fffedad5177b194f7107';
 var api_secret  = '7c2f09e6eb84e8a6183c59e0bc574f70';
 var url         = 'https://ws.audioscrobbler.com/2.0/';
@@ -40,29 +28,12 @@ var lastfm      = new LastFM({
     apiUrl    : url
 });
 
-// FUNCTIONS :
-// -----------
-/**
- * Called when the url of a tab changes.
- * @param {type} tabId
- * @param {type} changeInfo
- * @param {type} tab
- * @returns {undefined}
- */
 function checkForValidUrl(tabId, changeInfo, tab) {
-    // Show the page action in the tab if the URL contains 'last.fm' and 'user'
     if (tab.url.indexOf('last.fm/user/') > -1) {
         chrome.pageAction.show(tabId);
     }
 };
 
-/**
- * Performs an action with given action name.
- * @param {Object} request
- * @param {Object} sender
- * @param {function} sendResponse
- * @returns {boolean}
- */
 function doAction(request, sender, sendResponse) {
     if (request.action === 'user.getTopArtists') {
         lastfm.user.getTopArtists({
@@ -70,7 +41,6 @@ function doAction(request, sender, sendResponse) {
             limit   : request.params.limit
         }, {
             success : function(data) {
-                console.log("Successfully received last.fm data.");
                 sendResponse(data);
             },
             error   : function(error, msg) {
